5843c737ea861942617c5be3d05d16e0a7c8441a
JJRR5 <jjrr_5@hotmail.com>
2024-01-20 16:59:51 -0600

    [ADD] CRUD for restaurants model

--- src/controllers/restaurants.controller.js
+++ src/controllers/restaurants.controller.js
@@ -4,7 +4,7 @@ import {
 } from '../schemas/restaurants.js';
 import Restaurant from '../models/Restaurant.js';
 
-export const getRestaurants = async (req, res) => {
+export const getRestaurants = async (_, res) => {
    try {
       const restaurants = await Restaurant.findAll();
       res.json({ data: restaurants });
@@ -12,15 +12,62 @@ export const getRestaurants = async (req, res) => {
       res.status(500).json({ message: error.message });
    }
 };
+export const getRestaurant = async (req, res) => {
+   try {
+      const id = req.params?.id;
+      if (!id) throw new Error('id is required');
+      const restaurant = await Restaurant.findOne({ where: { id } });
+      res.json({ data: restaurant });
+   } catch (error) {
+      res.status(404).json({ message: error.message });
+   }
+};
 export const createRestaurant = async (req, res) => {
    const validation = validateRestaurant(req.body);
    if (validation.success) {
       try {
          const newRestaurant = await Restaurant.create(validation.data);
-         res.status(201).json({ data: newRestaurant });
+         return res.status(201).json({ data: newRestaurant });
       } catch (error) {
          console.log(error);
-         res.status(500).json({ message: error.message });
+         return res.status(500).json({ message: error.message });
       }
+   }
+   res.status(400).json({
+      message: 'Validation failed',
+      errors: validation.error.errors,
+   });
+};
+export const deleteRestaurant = async (req, res) => {
+   try {
+      const id = req.params?.id;
+      if (!id) throw new Error('id is required');
+      const restaurant = await Restaurant.findOne({ where: { id } });
+      if (!restaurant) throw new Error('restaurant not found');
+      await restaurant.destroy();
+      res.json({ data: restaurant });
+   } catch (error) {
+      res.status(404).json({ message: error.message });
+   }
+};
+export const updateRestaurant = async (req, res) => {
+   const validation = validatePartialRestaurant(req.body);
+   if (!validation.success) {
+      return res.status(400).json({
+         message: 'Validation failed',
+         errors: validation.error.errors,
+      });
+   }
+   try {
+      const id = req.params?.id;
+      if (!id) throw new Error('id is required');
+      const restaurant = await Restaurant.findOne({ where: { id } });
+      if (!restaurant) throw new Error('restaurant not found');
+      restaurant.set(validation.data);
+      const updatedRestaurant = await restaurant.save();
+      console.log(updateRestaurant);
+      res.json({ data: updatedRestaurant });
+   } catch (error) {
+      res.status(404).json({ message: error.message });
    }
 };
--- src/routes/restaurant.routes.js
+++ src/routes/restaurant.routes.js
@@ -1,15 +1,18 @@
 import { Router } from 'express';
 import {
    getRestaurants,
+   getRestaurant,
    createRestaurant,
+   deleteRestaurant,
+   updateRestaurant,
 } from '../controllers/restaurants.controller.js';
 const projectRoutes = Router();
 
 projectRoutes.get('/restaurants', getRestaurants);
-projectRoutes.get('/restaurants/:id', getRestaurants);
+projectRoutes.get('/restaurants/:id', getRestaurant);
 projectRoutes.get('/restaurants/statistics');
 projectRoutes.post('/restaurants', createRestaurant);
-projectRoutes.patch('/restaurants/:id');
-projectRoutes.delete('/restaurants/:id');
+projectRoutes.patch('/restaurants/:id', updateRestaurant);
+projectRoutes.delete('/restaurants/:id', deleteRestaurant);
 
 export default projectRoutes;
