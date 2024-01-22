39f9a251dff7792cbcb980c979786a832f695120
JJRR5 <jjrr_5@hotmail.com>
2024-01-21 22:10:13 -0600

    [CONF] cors middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 import express from 'express';
 import projectRoutes from './routes/restaurant.routes.js';
+import { ACCEPTED_ORIGINS, NETLIFY_REGEX } from './constants.js';
+import cors from 'cors';
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+   cors({
+      origin: (origin, callback) => {
+         if (!origin) return callback(null, true);
+         if (ACCEPTED_ORIGINS.includes(origin) || NETLIFY_REGEX.test(origin)) {
+            return callback(null, true);
+         }
+         return callback(new Error('Not allowed by CORS'));
+      },
+   })
+);
 app.use('/api', projectRoutes);
 
 export default app;
---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const ACCEPTED_ORIGINS = [
+   'http://localhost:5173',
+   'https://melp-restaurant.netlify.app',
+];
+export const NETLIFY_REGEX =
+   /^https?:\/\/(www\.)?(deploy-preview-\d+--)?alinza-demo\.netlify\.app(\/[^\s]*)?$/;
